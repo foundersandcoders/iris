bc0212671f2329e1c91feae48f43dc7a60e8f29d
Jason Warren <jason@foundersandcoders.com>
2026-01-14 16:25:25 +0000

    fix(tui terminalKit): use correct methods to grab, hide and show cursor

--- src/tui/app.ts
+++ src/tui/app.ts
@@ -5,15 +5,16 @@ import terminalKit from 'terminal-kit';
 import { Router } from "./utils/router";
 import { Dashboard } from './screens/dashboard';
 
-const term = terminalKit.terminal;
-
 interface TUIOptions {
   startCommand?: string;
   args?: string[];
 }
 
+const term = terminalKit.terminal;
+
 export class TUI {
   private router: Router;
+  
   constructor(private options: TUIOptions = {}) {
     this.router = new Router(term);
   }
@@ -21,14 +22,14 @@ export class TUI {
   async start() {
     this.initialize();
     this.registerScreens();
+    
     await this.router.push("dashboard");
   }
 
   private initialize() {
-    /* Full Screen */
     term.fullscreen(true);
-    term.hideCursor();
-    term.grabInput({ mouse: false });
+    term.hideCursor(true);
+    term.grabInput(true);
 
     /* Ctrl+C */
     term.on('key', (key: string) => {
@@ -38,7 +39,9 @@ export class TUI {
       }
     });
 
-    process.stdout.on('resize', () => { /* TODO: Handle this in Router */ });
+    process.stdout.on('resize', () => {
+      /* TODO: Handle this in Router */
+    });
   }
   
   private registerScreens() {
@@ -49,11 +52,7 @@ export class TUI {
 
   private cleanup() {
     term.fullscreen(false);
-    term.showCursor(); // TODO: Check terminalKit.terminal API
+    term.hideCursor(false); // TODO: Check terminalKit.terminal API
     term.grabInput(false);
   }
-
-  // private refresh() {
-  //   if (this.currentScreen) this.currentScreen.render();
-  // }
 }
