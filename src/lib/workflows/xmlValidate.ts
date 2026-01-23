7367da18cc3ab93fd6c08cc731efa70f7023dfa3
Jason Warren <jason@foundersandcoders.com>
2026-01-23 16:59:51 +0000

    feat: import workflow utils

--- src/lib/workflows/xmlValidate.ts
+++ src/lib/workflows/xmlValidate.ts
@@ -5,10 +5,13 @@
  */
 
 import { readFileSync } from 'fs';
+
+import { createStep, stepEvent, failedResult } from './utils';
 import { parseILR, type ParseResult } from '../utils/xml/xmlParser';
 import type { ILRMessage } from '../utils/xml/xmlGenerator';
 import type { SchemaRegistry } from '../schema/schemaInterpreter';
 import { validateValue } from '../schema/schemaValidator';
+
 import type { SchemaValidationIssue } from '../types/schemaValidation';
 import type {
 	ValidateInput,
@@ -26,13 +29,11 @@ const STEPS = {
 	validate: { id: 'validate', name: 'Validate Against Schema' },
 } as const;
 
-// |==================================================================|
-
 export async function* xmlValidateWorkflow(
 	input: ValidateInput
 ): AsyncGenerator<WorkflowStepEvent, WorkflowResult<ValidateOutput>, void> {
 	const startTime = Date.now();
 	const steps: WorkflowStep[] = [];
 
-	// Steps will go here
+	// LA LA LA DO SOME BORING SHIT TO AN XML FILE LA LA LA
 }
