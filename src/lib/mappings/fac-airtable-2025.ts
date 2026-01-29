afcf14d9b0d0e08445e582134b346663e99d7e9c
Jason Warren <jason@foundersandcoders.com>
2026-01-29 15:45:54 +0000

    feat: add elements to skeleton

--- src/lib/mappings/fac-airtable-2025.ts
+++ src/lib/mappings/fac-airtable-2025.ts
@@ -67,5 +67,114 @@ export const facAirtableMapping: MappingConfig = {
 			xsdPath: 'Message.Learner.Postcode',
 			transform: 'uppercaseNoSpaces',
 		},
+
+		// ==================== LEARNER: Optional Fields ====================
+
+		// Column 223: PrevLearnRefNum
+		{
+			csvColumn: 'PrevLearnRefNum',
+			xsdPath: 'Message.Learner.PrevLearnRefNumber',
+			transform: 'trim',
+		},
+
+		// Column 2: Previous UKPRN
+		{
+			csvColumn: 'Previous UKPRN',
+			xsdPath: 'Message.Learner.PrevUKPRN',
+			transform: 'stringToIntOptional',
+		},
+
+		// Column 5: Family name
+		{
+			csvColumn: 'Family name',
+			xsdPath: 'Message.Learner.FamilyName',
+			transform: 'trim',
+		},
+
+		// Column 4: Given name
+		{
+			csvColumn: 'Given name',
+			xsdPath: 'Message.Learner.GivenNames',
+			transform: 'trim',
+		},
+
+		// Column 7: Date of birth
+		{
+			csvColumn: 'Date of birth',
+			xsdPath: 'Message.Learner.DateOfBirth',
+			transform: 'isoDate',
+		},
+
+		// Column 8: NI number
+		{
+			csvColumn: 'NI number',
+			xsdPath: 'Message.Learner.NINumber',
+			transform: 'uppercaseNoSpaces',
+		},
+
+		// Column 13: Telephone number
+		{
+			csvColumn: 'Telephone number',
+			xsdPath: 'Message.Learner.TelNo',
+			transform: 'digitsOnly',
+		},
+
+		// ==================== LEARNING DELIVERY: Aim 1 Fields ====================
+
+		// Column 35: Aim type (programme aim 1)
+		{
+			csvColumn: 'Aim type (programme aim 1)',
+			xsdPath: 'Message.Learner.LearningDelivery.AimType',
+			transform: 'stringToInt',
+			aimNumber: 1,
+		},
+
+		// Column 36: Programme aim 1 Learning ref (with trailing space!)
+		{
+			csvColumn: 'Programme aim 1 Learning ref ',
+			xsdPath: 'Message.Learner.LearningDelivery.LearnAimRef',
+			transform: 'uppercase',
+			aimNumber: 1,
+		},
+
+		// Column 37: Start date (aim 1)
+		{
+			csvColumn: 'Start date (aim 1)',
+			xsdPath: 'Message.Learner.LearningDelivery.LearnStartDate',
+			transform: 'isoDate',
+			aimNumber: 1,
+		},
+
+		// Column 38: Planned end date (aim 1)
+		{
+			csvColumn: 'Planned end date (aim 1)',
+			xsdPath: 'Message.Learner.LearningDelivery.LearnPlanEndDate',
+			transform: 'isoDate',
+			aimNumber: 1,
+		},
+
+		// Column 39: Funding module (aim 1)
+		{
+			csvColumn: 'Funding module (aim 1)',
+			xsdPath: 'Message.Learner.LearningDelivery.FundModel',
+			transform: 'stringToInt',
+			aimNumber: 1,
+		},
+
+		// Column 42: Delivery postcode (aim 1)
+		{
+			csvColumn: 'Delivery postcode (aim 1)',
+			xsdPath: 'Message.Learner.LearningDelivery.DelLocPostCode',
+			transform: 'uppercaseNoSpaces',
+			aimNumber: 1,
+		},
+
+		// Column 61: Completion status (aim 1)
+		{
+			csvColumn: 'Completion status (aim 1)',
+			xsdPath: 'Message.Learner.LearningDelivery.CompStatus',
+			transform: 'stringToInt',
+			aimNumber: 1,
+		},
 	],
 };
